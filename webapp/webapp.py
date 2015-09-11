367bcd428ca2dd132b58d8eb9eba7ceaf5c2d9b4
ax003d <ax003d@gmail.com>
2015-09-12 01:01:47 +0800

    add webapp

---
+++ webapp/webapp.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "You are visiting throw: {}".format(request.url)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
