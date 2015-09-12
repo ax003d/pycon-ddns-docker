f1fa1768b4d8a1f1bb4986d91cabdba68c0adfae
ax003d <ax003d@gmail.com>
2015-09-12 15:34:53 +0800

    modify content

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "You are visiting throw: {}".format(request.url)
+    return "You are visiting: {}".format(request.url)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
